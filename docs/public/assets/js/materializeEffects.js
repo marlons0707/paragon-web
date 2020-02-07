93f290d83e196c8dbba45e127ef86598e956ad7e
jsaravia1 <jsaravia@tata.com.gt>
2020-02-07 13:54:42 -0600

    Carga de pagina web

---
+++ docs/public/assets/js/materializeEffects.js
@@ -0,0 +1,23 @@
+// ==================================================
+// Instancias de javascript que necesita materialize
+// ==================================================
+
+$('.datepicker').datepicker();
+$('.sidenav').sidenav();
+$('.fixed-action-btn').floatingActionButton();
+$('.modal').modal();
+$('.trigger-modal').modal();
+$('.materialboxed').materialbox();
+$('.tooltipped').tooltip();
+$('.collapsible').collapsible();
+$('.chips').chips();
+$('.dropdown-trigger').dropdown();
+
+// $('.perfectScroll').perfectScrollbar();
+
+// Toasts
+function AlertToast(message,icon,color){
+    var toastHTML = '<span>'+message+'&nbsp;</span><i class="material-icons'+' '+color+'-text'+'">'+icon+'</i>';
+    return M.toast({html: toastHTML, classes: 'rounded'});
+}
+
