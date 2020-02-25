a33e7e1f7ed2194308098aa4f4ba2c1549dc8b89
jsaravia1 <jsaravia@tata.com.gt>
2020-02-24 20:53:51 -0600

    Agregando envio de correos y primer entrega 100%

---
+++ app/public/assets/js/sendEmail.js
@@ -0,0 +1,14 @@
+console.log('hola mundo');
+
+(function(){
+    emailjs.init('user_AzgrtQy9jUnd9u66mxdo8');
+ })();
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate the contact number value
+        this.contact_number.value = Math.random() * 100000 | 0;
+        emailjs.sendForm('web_marlon_saravia', 'template_ls2K9dyn_clone', this);
+    });
+}
